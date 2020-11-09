3fc1a5bc4b4665ddcff802edbaf9441edf322aae
zahraa123880 <zahraa5160@gmail.com>
2020-11-09 19:30:02 +0200

    put project-13

---
+++ project-13/css/main.js
@@ -0,0 +1,14 @@
+/*global console, alert, prompt*/
+function conver() {
+    "use stract";
+    var nm1 = document.getElementById("num1").value,
+	    nm2 = document.getElementById("num2").value,
+		result = nm1 + " " + nm2;
+    
+	document.getElementById("test").innerHTML = " welcome to " + result + " in our page "; 
+}
+function img(){
+	"use stract";
+	document.getElementById("apper").innerHTML = '<img src="imgs/back3.jpg" style="width:450px;height:300px">' ;
+}
+	
