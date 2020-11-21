ca842dc4cd3ff997c26c884f780a8ffb60db26cb
zahraa123880 <zahraa5160@gmail.com>
2020-11-21 17:50:22 +0200

    put project-13

--- project-13/css/main.js
+++ project-13/css/main.js
@@ -27,31 +27,31 @@ function img(){
     switch (pict) {
         case 'image1':
             document.getElementById("pic").innerHTML = '<img src="imgs/back5.jpg" style="width:450px;height:300px">';
-            console.log(an);
+            console.log(pict);
             break;
         
         case 'image2':
             document.getElementById("pic").innerHTML = '<img src="imgs/back1.jpg" style="width:450px;height:300px">';
-            console.log(an);
+            console.log(pict);
             break;
 
         case 'image3':
             document.getElementById("pic").innerHTML = '<img src="imgs/back6.png" style="width:450px;height:300px">';
-            console.log(an);
+            console.log(pict);
             break;
 
         case 'image4':
             document.getElementById("pic").innerHTML = '<img src="imgs/back4.jpg" style="width:450px;height:300px">';
-            console.log(an);
+            console.log(pict);
             break;
 
         case 'image5':
             document.getElementById("pic").innerHTML = '<img src="imgs/back3.jpg" style="width:450px;height:300px">';
-            console.log(an);
+            console.log(pict);
             break;
          default:
             document.getElementById("pic").innerText = "sorry the photo is not found";
-            console.log(an);
+            console.log(pict);
             break;
 	}
 }
